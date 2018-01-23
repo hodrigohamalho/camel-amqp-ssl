84fbdb360817e9a38414d58d8a830118f7ba633c
Rodrigo Ramalho <rramalho@haxorslab.local>
2018-01-23 16:01:30 -0200

    comments and router step

--- openshift-setup/setup.sh
+++ openshift-setup/setup.sh
@@ -1,17 +1,23 @@
-#oc login -u system:admin
-#oc adm policy add-cluster-role-to-user cluster-admin admin
-#oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift
-#oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/amq/amq63-persistent-ssl.json -n openshift
-#oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/master/fis-image-streams.json -n openshift
+# OPTIONAL STEP, login with a super user to create resources in openshift template, alternatively
+# you could create in the project scope only with a normal user
+oc login -u system:admin
 
-#oc login -u developer
+# Create the imagestreams and templates, skip if you already have
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/jboss-image-streams.json -n openshift
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/amq/amq63-persistent-ssl.json -n openshift
+oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/master/fis-image-streams.json -n openshift
+
+# Login with a normal user
+oc login -u developer
+# Adjust the project name
 oc new-project amq-demo 
 oc create serviceaccount amq-service-account
 oc policy add-role-to-user view system:serviceaccount:amq-demo:amq-service-account
 oc secrets new amq-app-secret amq-server.ks
 oc secrets add sa/amq-service-account secret/amq-app-secret
 
-# Create the broker
+# Create the broker using a openshift template
+# Adjust the parameters with your own values
 oc new-app --template=amq63-persistent-ssl  \
     --param AMQ_SPLIT=false                 \
     --param APPLICATION_NAME="amq-broker"   \
@@ -25,3 +31,5 @@ oc new-app --template=amq63-persistent-ssl  \
     --param AMQ_KEYSTORE_PASSWORD=redhat    \
     --param AMQ_STORAGE_USAGE_LIMIT=10gb    \
     --param IMAGE_STREAM_NAMESPACE=openshift
+
+oc create route passthrough --service=amq-broker-amq-amqp-ssl
